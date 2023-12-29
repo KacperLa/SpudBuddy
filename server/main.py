aff5fe0a62ff29b1700c2a05af043b12f62d9d0a
KacperLa <klaskak@gmail.com>
2023-12-29 18:43:42 -0500

    addign urdf to web viewer

--- server/main.py
+++ server/main.py
@@ -245,7 +245,7 @@ actual_state_mmap = mmap_reader(SEMAPHORE_NAME_ACTUAL, MAP_NAME_ACTUAL, actual_s
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('path.html')
 
 def generate_data():
     data_json = config['robotState']
